d99ee291165c1cce99a9ec4e6758f644f5199aa7
gl_buaa <1186805566@qq.com>
2018-05-10 05:55:51 +0000

    Update 100_相同的树.py

--- 100_相同的树.py
+++ 100_相同的树.py
@@ -47,3 +47,15 @@
 #         :type q: TreeNode
 #         :rtype: bool
 #         """
+
+class Solution(object):
+    def isSameTree(self, p, q):
+        """
+        :type p: TreeNode
+        :type q: TreeNode
+        :rtype: bool
+        """
+
+        if p and q:
+            return p.val == q.val and self.isSameTree(p.left, q.left) and self.isSameTree(p.right, q.right)
+        return p == q
