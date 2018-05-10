a8ea3c255aa23ba7f3cce1de581c639a246e50f2
gl_buaa <1186805566@qq.com>
2018-05-10 04:59:45 +0000

    Update 100_相同的树.py

--- 100_相同的树.py
+++ 100_相同的树.py
@@ -31,3 +31,19 @@
 #         [1,2,1],   [1,1,2]
 
 # 输出: false
+
+
+# # Definition for a binary tree node.
+# # class TreeNode(object):
+# #     def __init__(self, x):
+# #         self.val = x
+# #         self.left = None
+# #         self.right = None
+
+# class Solution(object):
+#     def isSameTree(self, p, q):
+#         """
+#         :type p: TreeNode
+#         :type q: TreeNode
+#         :rtype: bool
+#         """
